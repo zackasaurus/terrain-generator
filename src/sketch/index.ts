75eb019cd28e82459ceb0a19cf43617e82782858
SUWAPA PUTTAPOONTRAKUL <Namnam@suwapas-air.attlocal.net>
2020-05-17 23:41:28 -0400

    Start random generation

--- src/sketch/index.ts
+++ src/sketch/index.ts
@@ -2,11 +2,6 @@ export const terrainGenerator = (p) => {
   let x = 100;
   let y = 100;
 
-  p.setup = () => {
-    p.createCanvas(600, 600, p.WEBGL);
-    // p.drawBackground();
-    // p.setupPosition();
-  };
   // p.drawBackground = () => {
   //   p.background(200);
   // };
@@ -25,15 +20,47 @@ export const terrainGenerator = (p) => {
   const cols = w / scale;
   const rows = h / scale;
 
-  p.draw = () => {
-    p.translate(-w / 2, -h / 2, 0);
-    p.background(0);
-    p.noFill();
-    p.stroke(255);
+  let terrain: number[][];
+
+  terrain = new Array(rows);
+  for (var i = 0; i < terrain.length; i++) {
+    terrain[i] = new Array(cols);
+  }
+
+  p.setup = () => {
+    p.createCanvas(600, 600, p.WEBGL);
+    // p.drawBackground();
+    // p.setupPosition();
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < cols; x++) {
-        p.rect(x * scale, y * scale, scale, scale);
+        console.log(x);
+        console.log(y);
+
+        terrain[x][y] = p.random(-10, 10);
       }
+    }
+  };
+
+  p.draw = () => {
+    p.rotateX(p.PI / 2.3);
+    p.translate(-w / 2, -h / 4, 0);
+    p.background(0);
+    p.stroke(255);
+    // p.fill(100);
+    // p.noFill();
+    // p.frameRate(1);
+    for (let y = 0; y < rows - 1; y++) {
+      p.beginShape(p);
+      for (let x = 0; x < cols; x++) {
+        // p.rect(x * scale, y * scale, scale, scale);
+        p.vertex(x * scale, y * scale, terrain[x][y]);
+
+        p.vertex(x * scale, (y + 1) * scale, terrain[x][y + 1]);
+        // p.vertex(x * scale, (y  1) * scale);
+        // p.vertex((x + 1) * scale, y * scale);
+        // p.vertex(x * scale, (y + 2) * scale);
+      }
+      p.endShape();
     }
   };
 
